70ea12e0ca995ba7bfc14aceec96e60489cfb65e
Oooska <evan.horne@oooska.com>
2016-06-21 18:18:12 -0400

    Book information is returned in a table format, added comments

--- packtpub.psm1
+++ packtpub.psm1
@@ -1,4 +1,32 @@
-﻿function New-PacktPubWebSession {
+﻿<#
+    .SYNOPSIS
+
+    Logs into PacktPub with the supplied email address and password.
+
+    .DESCRIPTION
+
+    New-PacktPubWebSession takes the supplied username and password and attempts to log into PacktPub.com.
+    If it is successful, it returns a WebSession object that contains the login cookie. 
+
+    If it is not successful, it throws an error containining the inner text from the 'messages error' class on the returned page.
+
+    .PARAMETER email
+
+    The email address of the PacktPub account
+
+    .PARAMETER password
+
+    The password of the PacktPub account
+
+    .EXAMPLE
+
+    $ws = New-PacktPubWebSession "email@somewhere.com" "mysupersecretpassword"
+
+    .NOTES
+
+    You will not need to call this function yourself. It is exported for use by Register-PacktPubFreeBookJob. 
+#>
+function New-PacktPubWebSession {
     param(
         [parameter(Mandatory=$true)]
         [string]$email,
@@ -11,13 +39,74 @@
     $form = getLoginForm -page $page -email $email -password $password
 
     $loggedInPage = Invoke-WebRequest https://www.packtpub.com/ -Method Post -Body $form -WebSession $sv
+    $errs = $loggedInPage.ParsedHtml.body.getElementsByClassName('messages error')
 
-    return $sv
+    if ($errs.length -eq 0){
+        return $sv
+    }
+
+    throw "Unable to log into PacktPub. Error: "+$errs[0].innerText
 }
 
+<#
+    .SYNOPSIS
 
-#Get-PacktPubFreeBook logs into the specified PacktPub account, claims the free daily book, and, if $download=$true, downloads
-#the book in the specified format to $savedir.
+    Returns a list of all books on the specified PacktPub account.
+
+    .DESCRIPTION
+
+    Get-PacktPubBooks returns a list of books out to the pipeline. The list consists of a hash table containing the title
+    of the book, the PacktPub ID of the book, and the WebSession value that allows access to the book.
+
+    .PARAMETER ws
+    The websession value to use when accessing the PacktPub servers. If it is not supplied, a prompt will request the email address 
+    password from the user.
+
+    .EXAMPLE
+
+    $books = Get-PacktPubBooks
+#>
+function Get-PacktPubBooks {
+    param (
+        [parameter()]
+        $ws=(New-PacktPubWebSession)
+    )
+
+    process {
+        $booksPage = Invoke-WebRequest https://www.packtpub.com/account/my-ebooks -WebSession $ws
+        $booksElemArray = $booksPage.ParsedHtml.body.getElementsByClassName('product-line')
+        
+        foreach($elem in $booksElemArray){
+            $title = $elem.getAttribute('title')
+            $id = $elem.getAttribute('nid')
+            if($title -ne $null -and $id -ne $null){
+                $title = $title.Substring(0, $title.length-8) #remove " [ebook]" from end
+                Write-Output (Select-BookObject -id $id -title $title -ws $ws)
+            }
+        }
+    }
+}
+
+<#
+    .SYNOPSIS
+
+    Claims the free book of the day on the specified account
+
+    .DESCRIPTION
+
+    Get-PacktPubFreeBook logs into the specified PacktPub account and claims the free daily book 
+
+    .PARAMETER ws
+    The websession value to use when accessing the PacktPub servers. If it is not supplied, a prompt will request the email address 
+    password from the user.
+
+    .EXAMPLE
+
+    Get-PacktPubClaimFreeBook
+    
+    To claim and download the book: 
+    Get-PacktPubDownloadBooks | Get-PacktPubDownloadBooks
+#>
 function Get-PacktPubClaimFreeBook {
     param (
         [parameter()]
@@ -32,25 +121,51 @@ function Get-PacktPubClaimFreeBook {
       
     #Claim free book - throws 404 error if not logged in successfully
     $claimBookPage = Invoke-WebRequest $claimURL -WebSession $ws
-    
-
     if($claimBookPage){
-        return @{
-            ws = $ws
-            id = $bookID
-            title = $title
-        }
+        return Select-BookObject $bookID $title $ws
     }
     
     throw "Error claiming book `"$title`" ($bookID)"
 }
 
+<#
+    .SYNOPSIS
+
+    Downloads the books specified by Get-PacktPubBooks or Get-PacktPubClaimFreeBook
+
+    .DESCRIPTION
+
+    Get-PacktPubDownloadBooks downloads the books that are passed in via the pipeline. Books can be 
+    downloaded as PDF, mobi, or epub, and saves the files to the current working directory unless
+    saveDir is specified. The filename  will consist of the title of the book, and the book id in parenthesis. 
+    If the title of the book has characters that are not valid for a filename (e.g. a colon ':'), they are stripped.
+
+    .PARAMETER books
+
+    The book information supplied by Get-PacktPubBooks or Get-PacktPubClaimFreeBook
+
+    .PARAMETER format
+
+    The format to download the books in. Currently supported values are PDF, mobi and epub.
+
+    .PARAMETER saveDir
+
+    The directory to save the book(s) to.
+
+    .EXAMPLE
+
+    To download the claimed book: 
+    Get-PacktPubDownloadBooks | Get-PacktPubDownloadBooks
+
+    To download all the books on your account:
+    Get-PacktPubBooks | Get-PacktPubDownloadBooks
+#>
 function Get-PacktPubDownloadBooks {
     param (
         [parameter(Mandatory=$true,
                    Position=0,
                    ValueFromPipeline=$true)]
-        [hashtable]$books,
+        $books,
 
         [parameter()] 
         [ValidateSet("pdf", "mobi", "epub")]
@@ -69,13 +184,57 @@ function Get-PacktPubDownloadBooks {
     process {
         foreach($book in $books){
             $bookID = $book.id
-            $bookTitle = $book.title
+            $bookTitle = (removeInvalidFilenameChars $book.title)
             Invoke-WebRequest "https://www.packtpub.com/ebook_download/$bookID/$format" -WebSession $book.ws -OutFile "$bookTitle ($bookID).$format"
         }
     }
 }
 
+<#
+    .SYNOPSIS
 
+    Creates a task to automatically acquire the free book of the day and download it at a specific time.
+
+    .DESCRIPTION
+
+    Register-PacktPubFreeBookJob creates a re-occuring task to acquire the free book, and then optionally may 
+    download the book in the specified format. Credentials are saved as plaintext in a file in %appdata%. 
+
+    The task will run when this function is first called, and then at 8PM local time there after.
+
+    By default, the computer will wake if it is sleeping or hibernating. Set WakeToRun to $false to override this feature.
+
+    If you want to cancel the task, you can use Get-ScheduledJob to view the list of powershell jobs, and Unregister-ScheduledJob to remove it
+
+    .PARAMETER email
+
+    The email address to use to log into packtpub.
+
+    .PARAMETER password
+
+    The password to use to log into packtpub.
+
+    .PARAMETER download
+
+    A bool indicating whether the task should download the book after adding it to the account.
+
+    .PARAMETER format
+
+    The format to download the books in. Currently supported values are PDF, mobi and epub.
+
+    .PARAMETER saveDir
+
+    The directory to save the book to. Default location is "Documents\ebooks" for the current user.
+
+    .PARAMETER time
+
+    The time to run the scheduled task. By default this runs at 8PM.
+
+    .PARAMETER WakeToRun
+
+    A boolean switch indicating whether the task should wake the computer from sleep/hibernate. The default is true.
+
+#>
 function Register-PacktPubFreeBookJob {
     #Registers a job to call Get-PacktPubFreeBook at the specified time
     #By default, the task will wake your computer if it is hibernating or sleeping in order to accomplish this task
@@ -128,7 +287,38 @@ function Register-PacktPubFreeBookJob {
     }
 }
 
+#Select-BookObject takes the id, title, and websession required to download a specific book and returns
+#a table-formatted object to be used with out-gridview or similar
+function Select-BookObject {
+    param(
+        [parameter()]
+        $id,
 
+        [parameter()]
+        $title,
+
+        [parameter()]
+        $ws
+    )
+
+    return Select-Object -InputObject @{id = $id; title = $title; ws = $ws}`
+            -Property @{Label="id"; Expression={$_.id}},`
+                      @{Label="title"; Expression={$_.title}},`
+                      @{Label="ws"; Expression={$_.ws}}
+}
+
+#removeInvalidChars takes a string, and removes characters that can't
+#be used in a filename
+function removeInvalidFilenameChars {
+    param(
+        [parameter()]
+        $filename
+    )
+   $arrInvalidChars = [System.IO.Path]::GetInvalidFileNameChars() 
+   
+   #'[' and ']' are not illegal characters, but Invoke-WebRequest doesn't like them
+   return $filename.Split($arrInvalidChars) -join "" -replace '[\[]','(' -replace '[\]]',')'  #Probably a more efficient way to do this
+}
 
 #getLoginForm returns a hash table representing the form that must be
 #POSTed to packtpub.
@@ -174,7 +364,6 @@ function getFreeBookTitle($dotdpage){
     return ($dotdpage.ParsedHtml.getElementsByTagName("div") | where{ $_.className -eq 'dotd-title'}).textContent.Trim()
 }
 
-
 #writeSettings saves the settings required by a scheduled job to %appdata%/packtpub.dat
 #TODO: Encrypt information (particularly password)
 function writeSettings($email, $password, $download, $format, $saveDir){
@@ -196,6 +385,7 @@ function Read-PacktPubSettings(){
 }
 
 Export-ModuleMember -Function New-PacktPubWebSession
+Export-ModuleMember -Function Get-PacktPubBooks
 Export-ModuleMember -Function Get-PacktPubClaimFreeBook
 Export-ModuleMember -Function Get-PacktPubDownloadBooks
 Export-ModuleMember -Function Register-PacktPubFreeBookJob
